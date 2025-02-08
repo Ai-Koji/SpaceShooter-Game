da5aed5ca201dc558d007cc8521c50061c5fb27a
Ivan <kireev.ivan.8@gmail.com>
2025-02-08 13:40:12 +0700

    added new parametres for enemt

--- SpaceShooter/Enemy.cs
+++ SpaceShooter/Enemy.cs
@@ -5,9 +5,11 @@ namespace SpaceShooter;
 
 public class Enemy : ScreenSurface, IMoveable
 {
-    public Enemy() : base(1, 1)
+    private int _ticks, _speed;
+    public Enemy(int speed) : base(1, 1)
     {
         Surface[0, 0].Glyph = 'V';
+        _speed = speed;
     }
 
     public void Move()
